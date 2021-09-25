52efe85729410819060db94a28c8d902bef23eb3
gingus55 <gingus55@hotmail.com>
2021-09-25 08:32:42 +0100

    prompts added to get criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,21 +13,33 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function getCriteria(){
+  const passwordLength = Number(window.prompt("Choose a password length", ""));
+  
+  const useLowercase = Boolean(window.prompt("Would you like to use lower case?", ));
+
+  const useUppercase = Boolean(window.prompt("Would you like to use upper case?", ));
+
+  const useNumeric = Boolean(window.prompt("Would you like to include numbers?", ));
+
+  const useSpecialchars= Boolean(window.prompt("Would you like to include special characters?", ));
+
+  return
+}
+
+
+
+
 
 function generatePassword(){
   //all my code needs to go here
   //this is to get my criteria
-  function getCriteria(){
-    const passwordLength = Number(window.prompt("Choose a password length", ""));
-
-  }
-
-
+  getCriteria()
   //these are my criteria strings
-  var lowerCase="abcdefghijklmnopqrstuvwxyz";
-  var upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numeric="0123456789";
-  var specialChars="" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  // var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  // var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // var numeric = "0123456789";
+  // var specialChars = " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-
+return
 }
